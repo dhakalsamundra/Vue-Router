590522bbbe507823bac8132d5bea15b1d9c8b41d
Samundra Dhakal <samundra.dhakal@tuni.fi>
2022-09-12 15:59:05 +0300

    change app

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link :to="{ name: 'About' }">About</router-link>
-    <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
-  </div>
-
-  <button @click="redirect">Redirect</button>
-  <button @click="back">Go back</button>
-  <button @click="forward">Go forward</button>
-
+  <Navbar />
   <router-view/>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
+
 export default {
-   methods: {
-    redirect() {
-      this.$router.push({ name: 'Home' })
-    },
-    back() {
-      this.$router.go(-1)
-    },
-    forward() {
-      this.$router.go(1)
-    }
-  }
+  components: { Navbar }
 }
 </script>
 
 <style>
+body {
+  background: #f2f2f2;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  max-width: 960px;
+  max-width: 600px;
   margin: 0 auto;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  text-decoration: none;
-  padding: 10px;
-  border-radius: 4px;
-}
-
-#nav a.router-link-exact-active {
-  color: white;
-  background: crimson;
-}
-
-button {
-  margin: 0 10px;
-  padding: 10px;
-  border: none;
-  border-radius: 4px;
+  color: #555;
 }
 </style>
